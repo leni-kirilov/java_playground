e6107dca0c3276ab0d70c470d06188e9d132be6f
Leni Kirilov <leni.kirilov@gmail.com>
2024-07-12 21:53:19 +0300

    HackerRank tasks about integers

---
+++ src/test/java/com/kirilov/interview/test/solutions/HackerRankTasksTests.java
@@ -0,0 +1,47 @@
+package com.kirilov.interview.test.solutions;
+
+import com.kirilov.interview.solutions.HackerRankTasks;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class HackerRankTasksTests {
+
+    @Test
+    public void oneElement() {
+        Assertions.assertEquals(5, HackerRankTasks.lonelyinteger(List.of(5)));
+    }
+
+    @Test
+    public void multipleElements() {
+        Assertions.assertEquals(5, HackerRankTasks.lonelyinteger(List.of(1, 5, 1)));
+    }
+
+    @Test
+    public void multipleElements2() {
+        Assertions.assertEquals(3, HackerRankTasks.lonelyinteger(List.of(1, 5, 5, 3, 1)));
+    }
+
+
+    @Test
+    public void element1() {
+        Assertions.assertEquals(0, HackerRankTasks.diagonalDifference(
+                        List.of(
+                                List.of(1)
+                        )
+                )
+        );
+    }
+
+    @Test
+    public void twoRows() {
+        Assertions.assertEquals(1, HackerRankTasks.diagonalDifference(
+                        List.of(
+                                List.of(1, 2),
+                                List.of(3, 5)
+                        )
+                )
+        );
+    }
+}
